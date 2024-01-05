28173f47ed42da30f315e1de34be0ace74337bc9
MargaritaAlvarezGonz <margarita.alvarez3105@gmail.com>
2024-01-05 04:56:15 -0600

    dependency injection

--- src/app/padre/padre.component.ts
+++ src/app/padre/padre.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServicioFamiliarService } from '../servicio-familiar.service';
 
 @Component({
   selector: 'app-padre',
   templateUrl: './padre.component.html',
   styleUrls: ['./padre.component.css']
 })
-export class PadreComponent {
+export class PadreComponent implements OnInit {
 valorContador: number= 0;
 mensajePadre = 'Esta es la manera de enviar mensajes desde el componente Padre al Hijo';
 mensajeRecibidodesdeHijo='';
+hermanoGrande?:string;
+nombre?:string;
+
+constructor(private _servicioFamiliar: ServicioFamiliarService){
+
+}
+
+ngOnInit(): void {
+    this._servicioFamiliar.setHermanoGrande('Juan');
+    this.nombre= this._servicioFamiliar.getHermanoGrande();
+}
 
 recibirMensaje($event:string){
   this.mensajeRecibidodesdeHijo =$event;
@@ -22,6 +34,13 @@ decrementarContador(){
   this.valorContador--;
 }
 
+saludar(){
+  this._servicioFamiliar.saludar(this._servicioFamiliar.getHermanoPequeno()|| '');
+}
+
+preguntar(){
+ console.log( this._servicioFamiliar.preguntarPorHijo() );
+}
 
 
 }
